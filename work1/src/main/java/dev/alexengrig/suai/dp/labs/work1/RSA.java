0584e6a405faa35a8a5637c0538e18fcb8b70aed
Grig Alex <alexengrigdev@gmail.com>
2020-01-20 16:36:49 +0300

    Add decomposition

--- work1/src/main/java/dev/alexengrig/suai/dp/labs/work1/RSA.java
+++ work1/src/main/java/dev/alexengrig/suai/dp/labs/work1/RSA.java
@@ -1,5 +1,8 @@
 package dev.alexengrig.suai.dp.labs.work1;
 
+import dev.alexengrig.suai.dp.labs.work1.RSA.RSAParameters.PrivateRSAParameters;
+import dev.alexengrig.suai.dp.labs.work1.RSA.RSAParameters.PublicRSAParameters;
+
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Scanner;
@@ -9,60 +12,115 @@ import static java.math.BigInteger.ONE;
 public class RSA {
     public static final SecureRandom RANDOM = new SecureRandom();
 
-    private final BigInteger publicKey;
-    private final BigInteger privateKey;
-    private final BigInteger modulus;
-
-    public RSA(int bitLength) {
-        this(new BigInteger("65537"), bitLength); // 2 ^ 16 + 1
-    }
-
-    public RSA(BigInteger key, int bitLength) {
-        publicKey = key; // e
-        BigInteger p, q, phi;
-        do {
-            p = BigInteger.probablePrime(bitLength / 2, RANDOM);
-            q = BigInteger.probablePrime(bitLength / 2, RANDOM);
-            phi = p.subtract(ONE).multiply(q.subtract(ONE)); // f(n) = (p - 1) * (q - 1)
-        } while (!publicKey.gcd(phi).equals(ONE) || p.equals(q)); // p != q and gcd(e, phi) = 1
-        privateKey = publicKey.modInverse(phi); // d = e ^ -1 mod f(n)
-        modulus = p.multiply(q); // n = p * q
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input bit length:");
         int bitLength = Integer.parseInt(scanner.nextLine());
+        System.out.println("Input message:");
         String text = scanner.nextLine();
-        RSA rsa = new RSA(bitLength);
-        System.out.println(rsa);
         byte[] bytes = text.getBytes();
         BigInteger message = new BigInteger(bytes);
-        BigInteger encrypt = rsa.encrypt(message);
-        BigInteger decrypt = rsa.decrypt(encrypt);
         System.out.println(" Original message: " + message);
+        RSAParameters parameters = new RSAParameters(bitLength);
+        System.out.println(parameters);
+        PublicRSAParameters publicParameters = parameters.getPublicParameters();
+        RSAEncoder encoder = new RSAEncoder(publicParameters);
+        BigInteger encrypt = encoder.encrypt(message);
         System.out.println("Encrypted message: " + encrypt);
+        PrivateRSAParameters privateParameters = parameters.getPrivateParameters();
+        RSADecoder decoder = new RSADecoder(privateParameters);
+        BigInteger decrypt = decoder.decrypt(encrypt);
         System.out.println("Decrypted message: " + decrypt);
     }
 
-    public BigInteger encrypt(BigInteger message) {
-        if (message.compareTo(modulus) >= 0) {
-            String info = String.format("Message '%s' is greater than or equal to modulus '%s'.", message, modulus);
-            throw new IllegalArgumentException(info);
+    public static class RSAParameters {
+        private final BigInteger publicKey;
+        private final BigInteger privateKey;
+        private final BigInteger modulus;
+
+        public RSAParameters(int bitLength) {
+            this(new BigInteger("65537"), bitLength); // 2 ^ 16 + 1
         }
-        return message.modPow(publicKey, modulus); // c = m ^ e mod n
+
+        public RSAParameters(BigInteger key, int bitLength) {
+            publicKey = key; // e
+            BigInteger p, q, phi;
+            do {
+                p = BigInteger.probablePrime(bitLength / 2, RANDOM);
+                q = BigInteger.probablePrime(bitLength / 2, RANDOM);
+                phi = p.subtract(ONE).multiply(q.subtract(ONE)); // f(n) = (p - 1) * (q - 1)
+            } while (!publicKey.gcd(phi).equals(ONE) || p.equals(q)); // gcd(e, phi) = 1 and p != q
+            privateKey = publicKey.modInverse(phi); // d = e ^ -1 mod f(n)
+            modulus = p.multiply(q); // n = p * q
+        }
+
+        public PublicRSAParameters getPublicParameters() {
+            return new PublicRSAParameters(this);
+        }
+
+        public PrivateRSAParameters getPrivateParameters() {
+            return new PrivateRSAParameters(this);
+        }
+
+        @Override
+        public String toString() {
+            return "RSAParameters {\n" +
+                    "\t publicKey = " + publicKey + ",\n" +
+                    "\tprivateKey = " + privateKey + ",\n" +
+                    "\t   modulus = " + modulus + "\n" +
+                    "}";
+        }
+
+        public static class PublicRSAParameters {
+            private final BigInteger key;
+            private final BigInteger modulus;
+
+            public PublicRSAParameters(RSAParameters parameters) {
+                this.key = parameters.publicKey;
+                this.modulus = parameters.modulus;
+            }
+        }
+
+        public static class PrivateRSAParameters {
+            private final BigInteger key;
+            private final BigInteger modulus;
+
+            public PrivateRSAParameters(RSAParameters parameters) {
+                this.key = parameters.privateKey;
+                this.modulus = parameters.modulus;
+            }
+        }
     }
 
-    public BigInteger decrypt(BigInteger ciphertext) {
-        return ciphertext.modPow(privateKey, modulus); // c = m ^ d mod n
+    public static class RSAEncoder {
+        private final BigInteger key;
+        private final BigInteger modulus;
+
+        public RSAEncoder(PublicRSAParameters parameters) {
+            this.key = parameters.key;
+            this.modulus = parameters.modulus;
+        }
+
+        public BigInteger encrypt(BigInteger message) {
+            if (message.compareTo(modulus) >= 0) {
+                String info = String.format("Message '%s' is greater than or equal to modulus '%s'.", message, modulus);
+                throw new IllegalArgumentException(info);
+            }
+            return message.modPow(key, modulus); // c = m ^ e mod n
+        }
     }
 
-    @Override
-    public String toString() {
-        return "RSA{\n" +
-                "\tpublicKey=" + publicKey + ",\n" +
-                "\tprivateKey=" + privateKey + ",\n" +
-                "\tmodulus=" + modulus + "\n" +
-                "}";
+    public static class RSADecoder {
+        private final BigInteger key;
+        private final BigInteger modulus;
+
+        public RSADecoder(PrivateRSAParameters parameters) {
+            this.key = parameters.key;
+            this.modulus = parameters.modulus;
+        }
+
+        public BigInteger decrypt(BigInteger ciphertext) {
+            return ciphertext.modPow(key, modulus); // c = m ^ d mod n
+        }
     }
 }
